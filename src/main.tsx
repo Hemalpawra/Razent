ce1875f51c46c00c5f396f8b2c57ff04d71e39c7
Hemalpawra <hemalpawra@gmail.com>
2026-09-11 00:11:26 +0530

    Make Clerk publishable key explicit

--- src/main.tsx
+++ src/main.tsx
@@ -12,7 +12,10 @@ import "./index.css"
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <EnvErrorBoundary>
-      <ClerkProvider afterSignOutUrl="/">
+      <ClerkProvider
+        publishableKey={import.meta.env.VITE_CLERK_PUBLISHABLE_KEY}
+        afterSignOutUrl="/"
+      >
       <App />
     </ClerkProvider>
     </EnvErrorBoundary>
